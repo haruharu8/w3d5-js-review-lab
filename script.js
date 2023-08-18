2a8b21682e62915372ce1781110653b20697cd4c
Kelly Siu <ksiu@LABA.PSWAS.LOCAL>
2023-08-18 09:59:10 -0700

    first commit, up to The Farm part

---
+++ script.js
@@ -0,0 +1,65 @@
+/*
+I)
+  A.
+    1. Use '=' operator
+    2. Use '=' operator
+    3. let var1 = 0;
+        let var2 = var1;
+    4.  Declaring is 'let, const, var' and saying this exists.
+        Assigning is giving it a value with a equals sign.
+       **** Defining is 'TO DO'
+    5. A outline/blueprint of your code and logic before writing. It is to streamline your ideas i.e. planning out your flexbox in a grid.
+    6. Depending on the complexity of your problem, the more complex it is the more time is needed to spend on planning out the problem.
+
+
+
+  B.
+    1. 
+  */
+let firstVariable;
+// 2.
+firstVariable = 'Hello World';
+// 3.
+firstVariable = 123;
+// 4.
+let secondVariable = firstVariable;
+// 5.
+secondVariable = 'Im hungry'
+// 6.
+console.log(firstVariable);
+//Should be 123 **TO DO FIX THIS AREA**
+// 7. 
+let yourName = 'BlackPink';
+let expression = 'Hello, my name is ';
+let greeting = expression + yourName;
+console.log(greeting)
+
+// C.
+// 1.
+
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+console.log(a < b);
+console.log(c > d);
+console.log('Name' == 'Name');
+
+console.log(true || false);
+console.log(false || false || false || false || false || true);
+console.log(!false || false); 
+console.log(e == "Kevin");
+console.log(a + b == c);
+console.log(a * a == d);
+console.log(48 == '48');
+
+// D. 
+
+let animal = "cow";
+if(animal == "cow"){
+    console.log("mooooo");
+}else{
+    console.log("Hey! You're not a cow");
+}
